39e9b56fd2458dd873fc019f94a3b10b1cb069ba
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-10-14 22:18:56 +0200

    Mario can collect flag
    +Player can collect flag
    +Player can restart and quit level when winning.
    +Player can see the time it took to complete the level.

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Flag : MonoBehaviour {
+    public GameObject VictoryScreen;
+    public TMP_Text timeText;
+    float time;
+    void LateUpdate () {
+        time = Time.time;
+    }
+    void PlayerCompletedLevel () {
+        if (VictoryScreen != null && timeText != null) {
+            //Add animation and sound here!
+            VictoryScreen.SetActive (true);
+            Time.timeScale = 0;
+            timeText.text = time.ToString ("Time: 0.0s");
+        }
+    }
+    void OnTriggerEnter2D (Collider2D col) {
+        if (col.gameObject.layer == LayerMask.NameToLayer ("Player")) {
+            PlayerCompletedLevel ();
+        }
+    }
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+    public void RestartLevel () {
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+        Time.timeScale = 1;
+    }
+    public void NextLevel () {
+        SceneManager.LoadScene (0, LoadSceneMode.Single); //Change number to the right one!
+        Time.timeScale = 1;
+    }
+    public void QuitGame () {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+
+    }
+}
